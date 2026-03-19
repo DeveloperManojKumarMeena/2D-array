52e38fd9c34ef319617f612068961d1f2bb06fe9
DeveloperManojKumarMeena <manojkumarmeena1996@outlook.com>
2026-03-19 08:04:51 +0530

    Traverse and transpose a 2D array;

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
  //Q. Best Of Approch X Pattern sum of a matrix
  let mat = [[1,1,1,1],[1,1,1,1],[1,1,1,1],[1,1,1,1]]
  let sum = 0;
-    let n = mat.length;
+     let n = mat.length;
 
     for(let i = 0; i<n;i++){
         sum+=mat[i][i]
@@ -10,4 +10,19 @@
             sum += mat[i][n - 1 - i];
         }
     }
-   console.log(sum)
+//    console.log(sum)
+   //Time Complexity = O(n)
+
+   //Q.Traversal of a 2d array?
+
+   let Qarr = [[1,2,3],[4,5,6]]
+   let ans = Array.from({length:Qarr[0].length},()=>Array(Qarr.length))
+   let l = Qarr.length;
+
+   for(let i=0;i<ans.length;i++){
+     
+    for(let j=0;j<ans[i].length;j++){
+        ans[i][j] = Qarr[j][i]
+    }
+   }
+   console.log(ans)
