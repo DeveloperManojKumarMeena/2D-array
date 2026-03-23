8e902ff4d7018e9ead00502afe4ff7b86c95e7ce
DeveloperManojKumarMeena <manojkumarmeena1996@outlook.com>
2026-03-23 08:13:32 +0530

    Give an M*N matrix,return all elements of the matrix in spiral order?

--- RotateIamge.js
+++ RotateIamge.js
@@ -1,15 +1,25 @@
-let arr = [[1,2,3],[4,5,6],[7,8,9]];
+let matrix = [[1,2,3],[4,5,6],[7,8,9]];
 /*
 Q. Rotate Image in plance 
 leetcode Q.no.48
 */
 
-for(let i =0; i<arr.length;i++){
-    for(let j=i;j<arr.length;j++){
-        let temp = arr[i][j]
-        arr[i][j]=arr[j][i]
-        arr[j][i]=temp
+for(let i =0; i<matrix.length;i++){
+    for(let j=i;j<matrix.length;j++){
+        let temp = matrix[i][j]
+        matrix[i][j]=matrix[j][i]
+        matrix[j][i]=temp
     }
     
 }
-console.log(arr)
+for(let i =0;i<matrix.length;i++){
+    let k =0 , j=matrix.length-1;
+    while(k<j){
+        let temp = matrix[i][k]
+        matrix[i][k]=matrix[i][j]
+        matrix[i][j]=temp
+        k++
+        j--
+    }
+}
+console.log(matrix)
---
+++ spiral.js
@@ -0,0 +1,11 @@
+/*
+Q.Give an M*N matrix,return all elements of the matrix in spiral order?
+[
+[1,2,3],
+[4,5,6],
+[7,8,9]
+]
+=> [1,2,3,6,9,8,7,4,5]
+*/
+let arr = [[1,2,3],[4,5,6],[7,8,9]];
+ 
