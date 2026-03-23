0a0a0b793d339e8f5db9c708c37fb0551fdb50bd
DeveloperManojKumarMeena <manojkumarmeena1996@outlook.com>
2026-03-23 09:43:13 +0530

    solve the problem of printing a matrix in spiral order. The code initializes the boundaries of the matrix and iteratively traverses the matrix in a spiral manner, updating the boundaries after each traversal. The final result is stored in the 'ans' array, which is then printed to the console.

--- spiral.js
+++ spiral.js
@@ -7,5 +7,29 @@ Q.Give an M*N matrix,return all elements of the matrix in spiral order?
 ]
 => [1,2,3,6,9,8,7,4,5]
 */
-let arr = [[1,2,3],[4,5,6],[7,8,9]];
- 
+let mat = [[1,2,3,4],[5,6,7,8],[9,10,11,12],[13,14,15,16]];
+
+let top = 0 , left = 0 ;
+let bottom = mat.length-1, right = mat[0].length-1;
+
+let ans = new Array;
+
+while(top<=bottom && left<=right){
+    for(let j=left;j<=right;j++){
+        ans.push(mat[top][j])
+    }
+    top++
+    for(let i = top; i<=bottom;i++){
+        ans.push(mat[i][right])
+    }
+    right--
+    for(let j=right;j>=left;j--){
+        ans.push(mat[bottom][j])
+    }
+    bottom--
+    for(let i = bottom; i>=top;i--){
+        ans.push(mat[i][left])
+    }
+    left++
+}
+console.log(ans)
